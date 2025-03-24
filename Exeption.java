b42eec5400d6b9fff7f606516998e4e030261d72
Shukl-amber <shukl.ember@gmail.com>
2025-03-24 13:45:55 +0530

    deleted:    Employee_Ret$1.class 	modified:   Exeption.class 	modified:   Exeption.java

--- Exeption.java
+++ Exeption.java
@@ -2,9 +2,12 @@ public class Exeption {
     public static void main(String[] args) {
         try {
             int[] arr = new int[5];
-            System.out.println(arr[4]);
+            arr[4]=10/0;
+            System.out.println(arr[5]);
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Error: ArrayIndexOutOfBoundsException");
+            System.out.println("Index Error");
+        } catch (ArithmeticException e){
+            System.out.println("Arithmatic Error");
         }
     }
 }
