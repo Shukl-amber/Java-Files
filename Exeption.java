91b8b2769247c42a8f04537d1b01cb63401f5b1e
Shukl-amber <shukl.ember@gmail.com>
2025-03-25 08:28:31 +0530

    modified:   Exeption.class 	modified:   Exeption.java

--- Exeption.java
+++ Exeption.java
@@ -2,12 +2,14 @@ public class Exeption {
     public static void main(String[] args) {
         try {
             int[] arr = new int[5];
-            arr[4]=10/0;
+            arr[4]=10/10;
             System.out.println(arr[5]);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Index Error");
         } catch (ArithmeticException e){
             System.out.println("Arithmatic Error");
+        } finally {
+            System.out.println("Final Block");
         }
     }
 }
