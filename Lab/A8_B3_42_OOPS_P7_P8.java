8cc0a13fe044bf4fd863636aa404a5caec09bc39
Shukl-amber <shukl.ember@gmail.com>
2025-04-15 09:28:34 +0530

    modified:   Lab/A8_B3_42_OOPS_P7_P8.java

--- Lab/A8_B3_42_OOPS_P7_P8.java
+++ Lab/A8_B3_42_OOPS_P7_P8.java
@@ -27,6 +27,14 @@ class Product {
         System.out.println("Max Discount: " + max_discount + "%,");
         System.out.println("Final Cost: " + getfinal_discount() + "\n");
     }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
 }
 
 public class A8_B3_42_OOPS_P7_P8 {
@@ -53,24 +61,25 @@ public class A8_B3_42_OOPS_P7_P8 {
         }
 
         Scanner scanner = new Scanner(System.in);
-        int choice=0;
+        int choice = 0;
 
         System.out.println("Enter 1 To Display All Products");
         System.out.println("Enter 2 To Display Products With 50% Discount And Final Cost:");
-        System.out.println("Enter 3 To Exit");
+        System.out.println("Enter 3 To Sort Products By Cost");
+        System.out.println("Enter 4 To Sort Products By Manufacturer");
+        System.out.println("Enter 5 To Exit");
 
-        while (choice != 3) {
+        while (choice != 5) {
             System.out.print("Enter your choice: ");
             choice = scanner.nextInt();
             switch (choice) {
                 case 1:
                     System.out.println("All Products:\n");
-                    Iterator<Product> iterator = products.iterator();
-                    while (iterator.hasNext()) {
-                        Product p = iterator.next();
+                    for (Product p : products) {
                         p.display_product();
                     }
                     break;
+
                 case 2:
                     System.out.println("Products with 50% Discount:");
                     for (Product p : products) {
@@ -79,9 +88,35 @@ public class A8_B3_42_OOPS_P7_P8 {
                         }
                     }
                     break;
+
                 case 3:
+                    Collections.sort(products, new Comparator<Product>() {
+                        public int compare(Product p1, Product p2) {
+                            return Double.compare(p1.getCost(), p2.getCost());
+                        }
+                    });
+                    System.out.println("Products sorted by cost:\n");
+                    for (Product p : products) {
+                        p.display_product();
+                    }
+                    break;
+
+                case 4:
+                    Collections.sort(products, new Comparator<Product>() {
+                        public int compare(Product p1, Product p2) {
+                            return p1.getManufacturer().compareTo(p2.getManufacturer());
+                        }
+                    });
+                    System.out.println("Products sorted by manufacturer:\n");
+                    for (Product p : products) {
+                        p.display_product();
+                    }
+                    break;
+
+                case 5:
                     System.out.println("Exiting...\n");
                     break;
+
                 default:
                     System.out.println("Invalid choice. Please try again.\n");
             }
